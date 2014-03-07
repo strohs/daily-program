eae0a2613d80560bdf21fc4137f2deaa2f31aae7
Cliff <strohs1@gmail.com>
2014-03-07 16:04:15 -0500

    added java version of Disemvoweling programming challenge

--- src/clojure/disemvowler/core.clj
+++ src/clojure/disemvowler/core.clj
@@ -1,12 +1,14 @@
 (ns disemvowler.core)
 
-;Disemvoweling means removing the vowels from text. (For this challenge, the letters a, e, i, o, and u are considered vowels, and the letter y is not.) The idea is to make text difficult but not impossible to read, for when somebody posts something so idiotic you want people who are reading it to get extra frustrated.
-;To make things even harder to read, we'll remove spaces too. For example, this string:
-;two drums and a cymbal fall off a cliff
-;can be disemvoweled to get:
-;twdrmsndcymblfllffclff
-;We also want to keep the vowels we removed around (in their original order), which in this case is:
-;ouaaaaoai
+;; Disemvoweling means removing the vowels from text. (For this challenge, the letters a, e, i, o, and u are considered
+;; vowels, and the letter y is not.) The idea is to make text difficult but not impossible to read, for when somebody
+;; posts something so idiotic you want people who are reading it to get extra frustrated.
+;; To make things even harder to read, we'll remove spaces too. For example, this string:
+;; two drums and a cymbal fall off a cliff
+;; can be disemvoweled to get:
+;; twdrmsndcymblfllffclff
+;; We also want to keep the vowels we removed around (in their original order), which in this case is:
+;; ouaaaaoai
 
 
 (def vowels #{\a \e \i \o \u})
@@ -20,8 +22,7 @@
 (defn disemvowler
   [strings]
   (doseq [s strings]
-    (println (apply str (remove-chars s vowels-spaces)))
+    (println (apply str (remove-vowels s vowels-spaces)))
     (println (apply str (filter vowels s)))
     (println "------------------------------")))
-
 
