ee1daa44b2a59074b128e4147b43c66a893b008b
Cliff <strohs1@gmail.com>
2014-04-29 12:45:22 -0400

    added more comments for ascii architect challenge

--- src/clojure/ascii_architect/core.clj
+++ src/clojure/ascii_architect/core.clj
@@ -1,5 +1,8 @@
 (ns ascii-architect.core)
 
+;;; Challenge 158  http://www.reddit.com/r/dailyprogrammer/comments/236va2/4162014_challenge_158_intermediate_part_1_the/
+
+;; sample input string
 (def ss "j3f3e3e3d3d3c3cee3c3c3d3d3e3e3f3fjij3f3f3e3e3d3d3c3cee3c3c3d3d3e3e3fj")
 
 (defn key->line
@@ -10,11 +13,16 @@
     (vec (drop (k keymap) ascii-line))))
 
 (defn ascii-line
-  "returns an ascii line corresponding to the key: k, and with n number of spaces appended"
+  "returns an ascii line (seq of characters) corresponding to the key: k, and with n number of spaces appended"
   [k n]
   (reduce conj (key->line k) (repeat n \space)))
 
-(defn build-column [[_ spaces letter]]
+(defn build-column
+  "build a single ascii coiumn with spaces added to the end of the column
+  spaces - integer indicating number of spaces to add
+  letter - string between a - j indicating the letter of the column to build
+  returns a seq of characters representing the ascii column"
+  [[_ spaces letter]]
   (let [s (if (empty? spaces) 0 (Integer/parseInt spaces))
         l (keyword letter)]
     (ascii-line (keyword l) s)))
