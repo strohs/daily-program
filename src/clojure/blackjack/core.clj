cd95a070a5f09e1aa009fbf2d84fccbef7d7291d
Cliff <strohs1@gmail.com>
2014-05-14 16:09:11 -0400

    cleaned up unused code

--- src/clojure/blackjack/core.clj
+++ src/clojure/blackjack/core.clj
@@ -2,17 +2,18 @@
 
 (def suits #{:C :D :H :S})
 (def ranks #{:2 :3 :4 :5 :6 :7 :8 :9 :10 :J :Q :K :A})
-(def tens #{:10 :J :Q :K})
 (def rank->val {:2 2 :3 3 :4 4 :5 5 :6 6 :7 7 :8 8 :9 9 :10 10 :J 10 :Q 10 :K 10 :A 11})
 
-(defn build-decks [n]
-  (for [i (range 0 n)
-        suit suits
+(defn build-deck []
+  (for [suit suits
         rank ranks]
     {:suit suit :rank rank}))
 
 (defn print-card [{suit :suit rank :rank}]
   (str (name suit) (name rank)))
+
+(defn print-hand [hand]
+  (map #(str (print-card %)) hand))
 
 (defn ace-count [cards]
   (count (filter #(= :A (:rank %)) cards)))
@@ -42,10 +43,10 @@
                   hand))) [] cards)
     (persistent! hands)))
 
-(let [ deck (shuffle (build-decks 1))
+(let [ deck (shuffle (flatten (repeatedly 2 build-deck)))
        hands (into-hands deck)
        blackjacks (count (filter blackjack? hands))
        bj-percent (with-precision 4 (* 100 (/ (bigdec blackjacks) (count hands))))]
   (doseq [hand hands]
-    (println hand))
+    (println (print-hand hand) (if (blackjack? hand) "blackjack!" "")))
   (println (format "%d hands with %d blackjacks at %.2f percent" (count hands) blackjacks bj-percent)))
