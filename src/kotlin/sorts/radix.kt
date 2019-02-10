b36eb77d61bf97e4d635b2887e456fd9c314a8bb
CliffS <strohs1@gmail.com>
2019-02-10 13:58:34 -0500

    simplified radix sort to no longer use buckets and to modify list in place

--- src/kotlin/sorts/radix.kt
+++ src/kotlin/sorts/radix.kt
@@ -8,17 +8,6 @@ import kotlin.random.Random
  * Author: Cliff
  */
 
-// build a map of 10 "buckets" to hold the integers based on their radix
-fun buildIntBuckets(): MutableMap<Int, MutableList<Int>> {
-    val dMap = LinkedHashMap<Int, MutableList<Int>>()
-    for (i in 0..9) {
-        dMap[i] = mutableListOf()
-    }
-    return dMap
-}
-
-// returns a count of the number of digits in 'i'
-fun digitLength (i: Int?): Int = i?.toString()?.length ?: 0
 
 // build a list of size 'size', containing random integers between min to max inclusive
 fun randomIntList (min: Int, max: Int, size: Int): MutableList<Int> {
@@ -27,47 +16,42 @@ fun randomIntList (min: Int, max: Int, size: Int): MutableList<Int> {
     return rands
 }
 
+// returns a count of the number of digits in 'i'
+fun length (i: Int?): Int = i?.toString()?.length ?: 0
+
 // get the largest count of digits in the 'ints' list
 fun maxDigitLength (ints: List<Int>): Int {
-    val i = ints.maxBy { digitLength(it) }
-    return digitLength(i)
+    val i = ints.maxBy { length(it) }
+    return length(i)
 }
 
-// get the value of the 'd'th digit in 'int', where the rightmost digit starts at 'd'=1...
-fun getDigit (int:Int, d:Int): Int {
-    if (d > digitLength(int) ) return 0
-    var rem = 0
-    var num = int
-    for (i in 0 until d) {
-        rem = num % 10
-        num /= 10
+// get the value of the 'd'th digit in 'int', where the rightmost digit starts at position 'd'=1...
+fun digitSelector (int:Int, d:Int): Int {
+    
+    if (d > length(int) )
+        return 0
+    else {
+        var rem = 0
+        var num = int
+        for (i in 0 until d) {
+            rem = num % 10
+            num /= 10
+        }
+        return rem
     }
-    return rem
 }
-
 
 fun main() {
-    val buckets = buildIntBuckets()
     // random integers to sort
-    var ints = randomIntList(0, 999, 50)
-    ints.forEach {
-        print(" $it ")
-    }
-    println("\n--------------------")
+    val ints = randomIntList(0, 999999, 1_000_000)
+    //ints.forEach { println(" $it ") }
+    //println()
 
-    var divisor = 10
     // 1. determine the max number of digits amongst all ints in the list and loop that number of times
-    for (i in 1..maxDigitLength(ints)) {
-        // 2. put the ints into appropriate buckets based on least significant digits
-        ints.forEach { buckets[ getDigit(it,i)]!!.add(it) }
-        // 3. remove ints from buckets (in digit order 0 to 9) and place them back into the list
-        ints.clear()
-        buckets.forEach { k, vals ->
-            ints.addAll( buckets[k]!!)
-            buckets[k]!!.clear()
-        }
-        divisor *= 10
+    for (digitPos in 1..maxDigitLength(ints)) {
+        // 2. sort the list by individual digits, moving from right to left
+        ints.sortBy { digitSelector( it, digitPos ) }
     }
     // 4. ints should now be sorted
-    ints.forEach { print(" $it ") }
+    ints.forEach { println(" $it ") }
 }
